fb214699662121b7a3777a702e3702e919e207dc
Khasan Khafizov <xozzslip@gmail.com>
2025-01-14 00:30:39 +0400

    remove specifying non-zero slot

--- examples/simple-storage.ts
+++ examples/simple-storage.ts
@@ -6,9 +6,6 @@ export function deploy(): void {
 export function main(): void {
     const sdk = new SDK();
     const slot = new Uint8Array(32); // slot 0
-    slot[0] = 'a'.codePointAt(0);
-    slot[11] = 'a'.codePointAt(0);
-    slot[31] = 'x'.codePointAt(0);
     if (sdk.inputSize() == 0) {
         const data = sdk.readStorage(slot);
         sdk.writeOutput(data);
