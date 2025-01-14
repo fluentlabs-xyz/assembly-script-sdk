e677b614ad2fcf81ed0d572280d1426317ac2a29
Khasan Khafizov <xozzslip@gmail.com>
2025-01-14 11:41:21 +0400

    since storage read returns 33 bytes, get subarray in test

--- examples/simple-storage.ts
+++ examples/simple-storage.ts
@@ -7,7 +7,7 @@ export function main(): void {
     const sdk = new SDK();
     const slot = new Uint8Array(32); // slot 0
     if (sdk.inputSize() == 0) {
-        const data = sdk.readStorage(slot);
+        const data = sdk.readStorage(slot).subarray(0, 32);
         sdk.writeOutput(data);
     } else {
         const input = sdk.readInput();
